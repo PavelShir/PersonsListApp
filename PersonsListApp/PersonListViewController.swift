f63ff27c5076bc8e4c481c8c87687f628b8f719a
Pavel Shirokiy <pshirokiy@gmail.com>
2022-08-31 08:47:19 +0300

    some change

--- PersonsListApp/PersonListViewController.swift
+++ PersonsListApp/PersonListViewController.swift
@@ -13,32 +13,39 @@ class PersonListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.rowHeight = 50
       
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        personsList.count
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//
+//
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        
-        return 0
+        personsList.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personName", for: indexPath)
 
-        // Configure the cell...
+        let contact = personsList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = contact.fullName
+        
+        cell.contentConfiguration = content
 
         return cell
     }
-    */
+    
 
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
